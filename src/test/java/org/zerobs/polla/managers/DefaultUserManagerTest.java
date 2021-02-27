4acd19e893cc0b41d032539c343ac60bf5ac7ff7
ayush shukla <ayushmw@gmail.com>
2021-02-27 17:52:02 +0530

    added failing ut

---
+++ src/test/java/org/zerobs/polla/managers/DefaultUserManagerTest.java
@@ -0,0 +1,12 @@
+package org.zerobs.polla.managers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DefaultUserManagerTest {
+    @Test
+    void test_bs() {
+        assertEquals(1, 2);
+    }
+}
