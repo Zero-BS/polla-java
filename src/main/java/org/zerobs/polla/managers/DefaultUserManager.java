36bb6c3157eaf251ae295faa073a417052c37082
ayush shukla <ayushmw@gmail.com>
2021-02-27 14:55:03 +0530

    added new dirty code

--- src/main/java/org/zerobs/polla/managers/DefaultUserManager.java
+++ src/main/java/org/zerobs/polla/managers/DefaultUserManager.java
@@ -12,42 +12,154 @@ public class DefaultUserManager implements UserManager {
 
     @Override
     public User add(User user) {
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
         return userRepository.save(user);
+    }
+
+    public boolean uselessMethodWoCoverage1(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage2(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage3(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage4(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage5(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage6(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage7(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage8(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage9(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage0(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage10(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage11(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage12(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage13(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage14(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage15(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage16(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage17(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage18(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage19(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
+    }
+    public boolean uselessMethodWoCoverage20(String s) {
+        if (s== null)
+            return true;
+        if (s.isEmpty())
+            return true;
+        return false;
     }
 }
