cd8c52bc3f7c5b3f36b02b616c3ec34e064d09f1
ayush shukla <ayushmw@gmail.com>
2021-07-11 10:16:57 +0530

    added spring boot integration test

---
+++ src/test/java/org/zerobs/polla/PollaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerobs.polla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
